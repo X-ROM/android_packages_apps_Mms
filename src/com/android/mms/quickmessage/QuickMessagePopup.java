d1755e3998e26962b505265dcbb884cf7dcb354c
kroz <46.kroz@gmail.com>
2014-02-03 06:50:00 -0200

    TRDS: Dark Mms
    PS2:
    - added SlimRoms License
    
    PS3:
    - fix quickpopup
    - fix numberdialog (both are hardcoded)
    
    PS4:
    - quickmessage font/divider theme support
    - quickmessage textfield changed to white
    - stretchable bubbles support
    
    PS5:
    - redirect colors of original layout xml
    
    PS6:
    - adjust text color of message counter and timestamp
    
    PS7:
    - fix chip recipient background
    
    Change-Id: I3e8b23a11620d8794370c26e2282e53cdcceeb6d

--- src/com/android/mms/quickmessage/QuickMessagePopup.java
+++ src/com/android/mms/quickmessage/QuickMessagePopup.java
@@ -174,6 +174,9 @@ public class QuickMessagePopup extends Activity implements
         mInputMethod = Integer.parseInt(prefs.getString(MessagingPreferenceActivity.INPUT_TYPE,
                 Integer.toString(InputType.TYPE_TEXT_VARIATION_SHORT_MESSAGE)));
 
+        mDarkTheme = mContext.getResources().getConfiguration().uiThemeMode
+                == Configuration.UI_THEME_MODE_HOLO_DARK;
+
         // Set the window features and layout
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dialog_quickmessage);
