688110e79d93f4dbee769a933e13ae53d681f4b2
ebol4anthr4x <ebol4anthr4x@gmail.com>
2014-05-18 13:12:26 -0700

    Mark messages read if they are on-screen when phone is unlocked
    Change-Id: I225a31bea9abe553c1466e070f4c9b50245f05c5

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -2631,6 +2631,13 @@ public class ComposeMessageActivity extends Activity
     }
 
     @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(!hasFocus) return;
+        mConversation.markAsRead(true);
+    }
+
+    @Override
     protected void onPause() {
         super.onPause();
 
